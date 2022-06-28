18aee4207f4b393d25cc9f2da3f5fe3dbb204438
franciskisiara <franciskisiara@gmail.com>
2022-06-28 19:05:56 +0300

    Created migration for storing company information

--- app/Validators/RegisterValidator.ts
+++ app/Validators/RegisterValidator.ts
@@ -5,11 +5,20 @@ export default class RegisterValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    company_name: schema.string(),
+    company_name: schema.string({}, [
+      rules.unique({
+        table: 'companies',
+        column: 'name',
+      })
+    ]),
 
     email: schema.string({}, [
       rules.trim(),
       rules.email(),
+      rules.unique({
+        table: 'users',
+        column: 'email', 
+      })
     ]),
 
     password: schema.string({}, [
