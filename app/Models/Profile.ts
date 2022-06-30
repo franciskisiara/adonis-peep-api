e7d27999f882156da1133ed974cd608e5e3e7af2
franciskisiara <franciskisiara@gmail.com>
2022-06-30 08:39:51 +0300

    Started work on the profiles

---
+++ app/Models/Profile.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column, HasMany, hasMany } from '@ioc:Adonis/Lucid/Orm'
+import Company from 'App/Models/Company'
+import ProfilePoint from './ProfilePoint'
+
+export default class Profile extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public company_id: number
+
+  @column.dateTime({ autoCreate: true, serializeAs: null, })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true, serializeAs: null, })
+  public updatedAt: DateTime
+
+  @belongsTo(() => Company, {
+    foreignKey: 'company_id'
+  })
+  public company: BelongsTo<typeof Company>
+
+  @hasMany(() => ProfilePoint, {
+    foreignKey: 'profile_id'
+  })
+  public points: HasMany<typeof ProfilePoint>
+}
---
+++ app/Models/ProfilePoint.ts
@@ -0,0 +1,44 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Company from 'App/Models/Company'
+import Technology from 'App/Models/Technology'
+import Profile from './Profile'
+
+export default class ProfilePoint extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column()
+  public profile_id: number
+
+  @column()
+  public technology_id: number
+
+  @column()
+  public backend_points: number
+
+  @column()
+  public frontend_points: number
+
+  @column()
+  public mobile_points: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(() => Profile, {
+    foreignKey: 'profile_id'
+  })
+  public profile: BelongsTo<typeof Profile>
+
+  @belongsTo(() => Technology, {
+    foreignKey: 'technology_id'
+  })
+  public technology: BelongsTo<typeof Technology>
+}
